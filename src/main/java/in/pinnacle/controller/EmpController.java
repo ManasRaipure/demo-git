a4837f1997d21d9bf2cf88284812ea63507e54ee
Isha6789 <sharmaisha15628@gmail.com>
2025-01-22 15:47:54 +0530

    by Isha

--- src/main/java/in/pinnacle/controller/EmpController.java
+++ src/main/java/in/pinnacle/controller/EmpController.java
@@ -1,8 +1,15 @@
 package in.pinnacle.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import ch.qos.logback.core.model.Model;
+import in.pinnacle.model.Emp;
+
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +19,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class EmpController {
     List <Emp> empList = new ArrayList<>();
+    
     @GetMapping("/emp")
     public String getEmp(@RequestParam("id") int id, Model model) {
-        Emp emp = empList.get(id);
-        model.addAttribute("emp", emp);
         return "emp";
         }
         @PutMapping("/emp")
@@ -23,13 +29,5 @@ public class EmpController {
             empList.set(emp.getId(), emp);
             return "redirect:/emp";
             }
-
-        
-    
- 
-  
-
-    
-
     
 }
