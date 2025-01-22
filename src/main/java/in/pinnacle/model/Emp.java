d92ee540c8fefbdfda661367b4d50e20ac0fc194
Isha6789 <sharmaisha15628@gmail.com>
2025-01-22 15:06:44 +0530

    add Emp model and EmpController class

---
+++ src/main/java/in/pinnacle/model/Emp.java
@@ -0,0 +1,15 @@
+package in.pinnacle.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Emp {
+    private int id;
+    private String name;
+    private int age;
+
+}
